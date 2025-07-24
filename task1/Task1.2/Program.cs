fb8d133e9b7ac972b79b5ce54ea485b9efa9c63c
Renad Emad <renadalshooni@gmail.com>
2025-07-24 19:37:47 +0300

    Add files via upload

---
+++ task1/Task1.2/Program.cs
@@ -0,0 +1,49 @@
+﻿//Task2 Printing numbers from 1 to n
+// erasing comma in the end of the output
+//optimize solution using StringBuilder
+
+
+using System.Diagnostics;
+using System.Text;
+
+string? input;
+int n;
+// Input validation
+do
+{
+    Console.WriteLine("Enter a positive valid integer n:");
+    input = Console.ReadLine();
+} while (string.IsNullOrEmpty(input) || !int.TryParse(input, out n) || n <= 0); 
+
+Console.WriteLine("Printing numbers from 1 to n using ");
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+// O(n log n) solution using for loop
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(i);
+    if (i < n) // Avoid printing comma after the last number
+    {
+        Console.Write(", ");
+    }
+}
+stopwatch.Stop();
+Console.WriteLine($"Time taken for printing numbers from 1 to {n} using for loop: {stopwatch.Elapsed.TotalMilliseconds} ms");
+// using StringBuilder for better performance
+
+StringBuilder stringbulider = new StringBuilder();
+stopwatch.Reset();
+//O(n) solution using StringBuilder
+for (int i = 1; i <= n; i++)
+{
+    stringbulider.Append($"i");
+    if (i < n) // Avoid printing comma after the last number
+    {
+        stringbulider.Append(", ");
+    }
+}
+
+Console.WriteLine(stringbulider.ToString());
+stopwatch.Stop();
+
+Console.WriteLine($"Time taken for printing numbers from 1 to {n} using StringBuilder: {stopwatch.Elapsed.TotalMilliseconds} ms");
---
+++ task1/task1/Program.cs
@@ -0,0 +1,48 @@
+﻿//task 1 summation of numbers from 1 to n  
+//case 1 : using for loop
+//case 2 : using formula (n * (n + 1) / 2)
+//adding validation for value of n
+
+using System.Diagnostics;
+
+
+// ? ==> accept null value
+
+string? input;
+int n;
+
+// Input validation
+do
+{
+    Console.WriteLine("Enter a positive valid integer n:");
+    input = Console.ReadLine();
+}
+while (string.IsNullOrEmpty(input) || !int.TryParse(input, out n) || n <= 0);
+
+Stopwatch stopwatch = new Stopwatch();
+
+// First solution: using for loop
+int sum = 0;
+
+stopwatch.Start();
+
+for (int i = 1; i <= n; i++)
+{
+    sum += i;
+}
+stopwatch.Stop();
+
+Console.WriteLine($"Time taken for for loop: {stopwatch.Elapsed.TotalMilliseconds} ms");
+Console.WriteLine($"Sum of numbers from 1 to {n} using for loop is: {sum}");
+
+// Reset stopwatch 
+stopwatch.Reset();
+
+// Second solution: using formula
+stopwatch.Start();
+
+int formulaSum = n * (n + 1) / 2;
+
+stopwatch.Stop();
+Console.WriteLine($"Time taken for formula calculation: {stopwatch.Elapsed.TotalMilliseconds} ms");
+Console.WriteLine($"Sum of numbers from 1 to {n} using formula is: {formulaSum}");
